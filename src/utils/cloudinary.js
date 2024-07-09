1e02d5034dc9967b349bbf7fa1515363f9716393
Sunil kushwaha <33691873+sunilkush@users.noreply.github.com>
2024-07-09 13:48:08 +0530

    Create cloudinary.js

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,33 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
+
+cloudinary.config({
+    secure: true,
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: CLOUDINARY_API_KEY,
+    api_secret: CLOUDINARY_API_SECRET
+})
+
+const uploadOnCloudinary = async (localFilePath) => {
+
+    try {
+        if (!localFilePath) return null
+
+        const response = await cloudinary.uploader.upload(localFilePath, {
+            resource_type: "auto"
+        })
+
+        fs.unlinkSync(localFilePath)
+
+        return response
+
+    } catch (error) {
+        fs.unlinkSync(localFilePath)
+        return null
+
+    }
+
+}
+
+
+export { uploadOnCloudinary }
